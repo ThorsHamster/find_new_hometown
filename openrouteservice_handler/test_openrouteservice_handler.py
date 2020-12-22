362a6959d6af32a830d906cd770248f5be762fde
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-12-22 19:10:20 +0100

    Use pytest-mock instead of mock

--- openrouteservice_handler/test_openrouteservice_handler.py
+++ openrouteservice_handler/test_openrouteservice_handler.py
@@ -1,6 +1,4 @@
 import pytest
-from unittest import TestCase
-from unittest.mock import patch
 
 from openrouteservice_handler import OpenRouteServiceHandler
 
@@ -11,27 +9,27 @@ class MockCoordinate:
         self.latitude = 0
 
 
-class TestOpenRouteServiceHander(TestCase):
+@pytest.fixture
+def unit_under_test(mocker):
+    mocker.patch('openrouteservice_handler.openrouteservice_handler.YmlReader.read', return_value={'api_key': 10})
+    return OpenRouteServiceHandler()
 
-    @patch('openrouteservice_handler.openrouteservice_handler.YmlReader.read')
-    def setUp(self, mock_ymlreader):
-        mock_ymlreader.return_value = {'api_key': 10}
-        self.unit_under_test = OpenRouteServiceHandler()
 
-    @patch('openrouteservice_handler.openrouteservice_handler.openrouteservice.Client.distance_matrix')
-    def test_get_distance_duration_between_cities_standard_layout(self, mock_distance_matrix):
-        mock_distance_matrix.return_value = {'distances': [[0, 3], [0, 0]], 'durations': [[0, 7], [0, 0]]}
+def test_get_distance_duration_between_cities_standard_layout(unit_under_test, mocker):
+    mocker.patch('openrouteservice_handler.openrouteservice_handler.openrouteservice.Client.distance_matrix',
+                 return_value={'distances': [[0, 3], [0, 0]], 'durations': [[0, 7], [0, 0]]})
 
-        distance, duration = self.unit_under_test.get_distance_duration_between_cities(MockCoordinate(),
-                                                                                       MockCoordinate())
-        assert distance == 3
-        assert duration == 7
+    distance, duration = unit_under_test.get_distance_duration_between_cities(MockCoordinate(),
+                                                                              MockCoordinate())
+    assert distance == 3
+    assert duration == 7
 
-    @patch('openrouteservice_handler.openrouteservice_handler.openrouteservice.Client.distance_matrix')
-    def test_get_distance_duration_between_cities_switched_layout(self, mock_distance_matrix):
-        mock_distance_matrix.return_value = {'distances': [[0, 0], [3, 0]], 'durations': [[0, 0], [7, 0]]}
 
-        distance, duration = self.unit_under_test.get_distance_duration_between_cities(MockCoordinate(),
-                                                                                       MockCoordinate())
-        assert distance == 3
-        assert duration == 7
+def test_get_distance_duration_between_cities_switched_layout(unit_under_test, mocker):
+    mocker.patch('openrouteservice_handler.openrouteservice_handler.openrouteservice.Client.distance_matrix',
+                 return_value={'distances': [[0, 0], [3, 0]], 'durations': [[0, 0], [7, 0]]})
+
+    distance, duration = unit_under_test.get_distance_duration_between_cities(MockCoordinate(),
+                                                                              MockCoordinate())
+    assert distance == 3
+    assert duration == 7
