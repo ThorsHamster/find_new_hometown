738abaeb6ee2aae6981f5bd5aa0a07767eee7b25
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-12-28 12:59:20 +0100

    Refactoring of mock statements

--- sql_handler/test_sql_handler.py
+++ sql_handler/test_sql_handler.py
@@ -12,7 +12,7 @@ def unit_under_test(mocker):
 
 def test_connect_database_does_not_exist(unit_under_test, mocker):
     mocker.patch('os.path.isfile', return_value=False)
-    mocker.patch('sql_handler.sql_handler.sqlite3.connect')
+    mocker.patch('sql_handler.sql_handler.sqlite3')
 
     unit_under_test.connect()
 
@@ -21,8 +21,8 @@ def test_connect_database_does_not_exist(unit_under_test, mocker):
 
 def test_create_database_raises_error(unit_under_test, mocker):
     mocker.patch('os.path.isfile', return_value=False)
-    mock_sql = mocker.patch('sql_handler.sql_handler.sqlite3.connect')
-    mock_sql.side_effect = Error('Test')
+    mock_sql = mocker.patch('sql_handler.sql_handler.sqlite3')
+    mock_sql.connect.side_effect = Error('Test')
 
     with pytest.raises(Error, match='Test'):
         unit_under_test.connect()
@@ -32,7 +32,7 @@ def test_create_database_raises_error(unit_under_test, mocker):
 
 def test_connect_but_already_connected(unit_under_test, mocker):
     mocker.patch('os.path.isfile', return_value=True)
-    mocker.patch('sql_handler.sql_handler.sqlite3.connect')
+    mocker.patch('sql_handler.sql_handler.sqlite3')
 
     unit_under_test.connect()
     unit_under_test.connect()
@@ -42,7 +42,7 @@ def test_connect_but_already_connected(unit_under_test, mocker):
 
 def test_connect_database_does_already_exist(unit_under_test, mocker):
     mocker.patch('os.path.isfile', return_value=True)
-    mocker.patch('sql_handler.sql_handler.sqlite3.connect')
+    mocker.patch('sql_handler.sql_handler.sqlite3')
 
     unit_under_test.connect()
 
@@ -50,7 +50,7 @@ def test_connect_database_does_already_exist(unit_under_test, mocker):
 
 
 def test_close_not_closed(unit_under_test, mocker):
-    mocker.patch('sql_handler.sql_handler.sqlite3.connect')
+    mocker.patch('sql_handler.sql_handler.sqlite3')
 
     unit_under_test.connect()
     unit_under_test.close()
@@ -59,7 +59,7 @@ def test_close_not_closed(unit_under_test, mocker):
 
 
 def test_close_already_closed(unit_under_test, mocker):
-    mocker.patch('sql_handler.sql_handler.sqlite3.connect')
+    mocker.patch('sql_handler.sql_handler.sqlite3')
 
     unit_under_test.connect()
     unit_under_test.close()
@@ -69,8 +69,8 @@ def test_close_already_closed(unit_under_test, mocker):
 
 
 def test_get_coordinates_from_city(unit_under_test, mocker):
-    mock_sql = mocker.patch('sql_handler.sql_handler.sqlite3.connect')
-    mock_sql.return_value.cursor.return_value.fetchone.return_value = [10, 11]
+    mock_sql = mocker.patch('sql_handler.sql_handler.sqlite3')
+    mock_sql.connect().cursor().fetchone.return_value = [10, 11]
 
     coordinates = unit_under_test.get_coordinates_from_city('test_city')
 
@@ -79,8 +79,8 @@ def test_get_coordinates_from_city(unit_under_test, mocker):
 
 
 def test_set_distance_duration_unknown_city(unit_under_test, mocker):
-    mock_sql = mocker.patch('sql_handler.sql_handler.sqlite3.connect')
-    mock_sql.return_value.cursor.return_value.fetchone.return_value = None
+    mock_sql = mocker.patch('sql_handler.sql_handler.sqlite3')
+    mock_sql.connect().cursor().fetchone.return_value = None
 
     with pytest.raises(ValueError, match='City not known.'):
         unit_under_test.set_distance_duration('unknown_city', 'test_city2', 0, 0)
@@ -95,17 +95,17 @@ def test_set_coordinates_from_city(unit_under_test, mocker):
 
 
 def test_set_distance_duration(unit_under_test, mocker):
-    mock_sql = mocker.patch('sql_handler.sql_handler.sqlite3.connect')
-    mock_sql.return_value.cursor.return_value.fetchone.side_effect = [[10], [11]]
+    mock_sql = mocker.patch('sql_handler.sql_handler.sqlite3')
+    mock_sql.connect().cursor().fetchone.side_effect = [[10], [11]]
 
     unit_under_test.set_distance_duration('test_city', 'test_city2', 30, 40)
 
-    assert any("10, 11, 30, 40" in str(c) for c in mock_sql.mock_calls)
+    assert mock_sql.connect().cursor().execute.assert_called_with(ANY, (10, 11, 30, 40)) is None
 
 
 def test_get_value_distance(unit_under_test, mocker):
-    mock_sql = mocker.patch('sql_handler.sql_handler.sqlite3.connect')
-    mock_sql.return_value.cursor.return_value.fetchone.side_effect = [[10], [11], [45]]
+    mock_sql = mocker.patch('sql_handler.sql_handler.sqlite3')
+    mock_sql.connect().cursor().fetchone.side_effect = [[10], [11], [45]]
 
     return_value = unit_under_test.get_value('test_city', 'test_city2', 'distance')
 
@@ -113,8 +113,8 @@ def test_get_value_distance(unit_under_test, mocker):
 
 
 def test_get_value_duration(unit_under_test, mocker):
-    mock_sql = mocker.patch('sql_handler.sql_handler.sqlite3.connect')
-    mock_sql.return_value.cursor.return_value.fetchone.side_effect = [[10], [11], [54]]
+    mock_sql = mocker.patch('sql_handler.sql_handler.sqlite3')
+    mock_sql.connect().cursor().fetchone.side_effect = [[10], [11], [54]]
 
     return_value = unit_under_test.get_value('test_city', 'test_city2', 'duration')
 
@@ -122,8 +122,8 @@ def test_get_value_duration(unit_under_test, mocker):
 
 
 def test_get_value_unknown_city(unit_under_test, mocker):
-    mock_sql = mocker.patch('sql_handler.sql_handler.sqlite3.connect')
-    mock_sql.return_value.cursor.return_value.fetchone.side_effect = [[10], [11], None]
+    mock_sql = mocker.patch('sql_handler.sql_handler.sqlite3')
+    mock_sql.connect().cursor().fetchone.side_effect = [[10], [11], None]
 
     return_value = unit_under_test.get_value('unkown_city', 'test_city2', 'duration')
 
@@ -131,8 +131,8 @@ def test_get_value_unknown_city(unit_under_test, mocker):
 
 
 def test_get_value_option_not_allowed(unit_under_test, mocker):
-    mock_sql = mocker.patch('sql_handler.sql_handler.sqlite3.connect')
-    mock_sql.return_value.cursor.return_value.fetchone.side_effect = [[10], [11], None]
+    mock_sql = mocker.patch('sql_handler.sql_handler.sqlite3')
+    mock_sql.connect().cursor().fetchone.side_effect = [[10], [11], None]
 
     with pytest.raises(ValueError, match='Only "distance" and "duration" allowed.'):
         unit_under_test.get_value('test_city1', 'test_city2', 'not_allowed')
