1e6c1b92c61c28387bb990c969d47584c4c71764
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-12-28 13:40:10 +0100

    Patch also YmlReader

--- test_main.py
+++ test_main.py
@@ -2,6 +2,11 @@ from main import run_home_town_finder
 
 
 def test_script_execution(mocker):
+    mocker.patch('home_town_finder.home_town_finder.YmlReader.read', side_effect=[{'cities': 'a'},
+                                                                                  {'api_key': 10,
+                                                                                   'target_city_1': 'city_A',
+                                                                                   'target_city_2': 'city_B',
+                                                                                   'option': 'duration'}])
     mock_home_town_finder = mocker.patch('main.HomeTownFinder.run')
 
     run_home_town_finder()
