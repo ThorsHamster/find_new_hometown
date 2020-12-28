f0ef196b0d2fd009161bb768a8ea9be3b7270d97
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-12-28 13:36:22 +0100

    Use assert_called_once instead of any-search

--- test_main.py
+++ test_main.py
@@ -2,8 +2,8 @@ from main import run_home_town_finder
 
 
 def test_script_execution(mocker):
-    mock_home_town_finder = mocker.patch('main.HomeTownFinder')
+    mock_home_town_finder = mocker.patch('main.HomeTownFinder.run')
 
     run_home_town_finder()
 
-    assert any("run" in str(c) for c in mock_home_town_finder.mock_calls)
+    mock_home_town_finder.assert_called_once()
